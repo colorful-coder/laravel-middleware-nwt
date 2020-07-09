707ba691fcd9d5412f4e338c7d693fc0135b8be0
nwenwewinthan777 <scm.nwenwewinthan@gmail.com>
2020-07-10 02:06:02 +0630

    add middleware to check admin user

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ADMIN PAGE</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container py-5">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">WELCOME TO ADMIN ROUTE</div>
+            <div class="panel-body py-3 pl-4">
+                <a href="{{ route('home') }}">Home</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route middleware
+// Route::get('admin/routes', 'HomeController@admin')->middleware('admin')->name('admin');
+
+// Group middleware
+// Route::group(['middleware' => ['admin']], function () {
+//     Route::get('admin/routes', 'HomeController@admin')->name('admin');
+// });
+
+// Multiple Middlewares in Single Route
+Route::get('admin/routes', 'HomeController@admin')->middleware(['auth','admin'])->name('admin');
